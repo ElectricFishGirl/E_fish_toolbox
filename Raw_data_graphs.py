a95461fe079bd6fb60b17e665a63fb0b7a273535
Lewis Lab <berradamia@gmail.com>
2019-09-12 15:49:24 -0400

    More general graphs

--- Raw_data_graphs.py
+++ Raw_data_graphs.py
@@ -12,17 +12,17 @@ import os
 
 fish_names = helpers.get_all_fish(helpers.RECORDING_PATH16) # CHANGE TO RECORDING_PATH8 for 8 bits files
 indexes = []
-
-fish = fish_names[0]
+sub = 100
+fish = fish_names[2]
 for fish in (fish_names):
     mat_files = helpers.get_mat_files(fish, helpers.RECORDING_PATH16)
-    file = mat_files[0]
+    file = mat_files[2]
     shift = 0
-    for file in mat_files[0:5]:
+    for file in mat_files[0:4]:
         data = helpers.load_mat(file)
         file_name = helpers.path_to_name(file)
-        sub_data = data[::100]
-        sampling_frequency = helpers.MAT_FREQUENCY/100
+        sub_data = data[::sub]
+        sampling_frequency = helpers.MAT_FREQUENCY/sub
         t_max = int(len(data) / helpers.MAT_FREQUENCY)
         x = np.arange(0, t_max, 1 / sampling_frequency)
         y = sub_data
@@ -36,7 +36,7 @@ for fish in (fish_names):
         plt.xlabel('Frequency ')
         plt.xlim(0,6000)
         plt.title('FFT ' + file_name )
-    helpers.save_figure(join(helpers.SAVE_PATH, fish), 'FFT  ', file_name)
+    helpers.save_figure(join(helpers.SAVE_PATH, fish), 'FFT', fish)
     plt.close()
 
 
