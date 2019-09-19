684a722b91736e9f6d55b85e2492b0e19d98ebd0
ElectricFishGirl <berradamia@gmail.com>
2019-09-19 16:25:17 -0400

    kj

--- temporary_run16.py
+++ temporary_run16.py
@@ -10,7 +10,7 @@ indexes = []
 fish = fish_names[0]
 for fish in fish_names:
     mat_files = helpers.get_mat_files(fish, helpers.RECORDING_PATH16)
-    file = mat_files[14]
+    file = mat_files[10]
     for file in mat_files[3:15]:
         data = helpers.load_mat(file)
         #data = np.array(data, dtype='int')
@@ -18,7 +18,7 @@ for fish in fish_names:
         sampling_frequency = helpers.MAT_FREQUENCY
         t_max = len(data) / helpers.MAT_FREQUENCY
         time_array = np.arange(0, t_max-1/sampling_frequency, 1 / sampling_frequency)  # IMPLEMENT IN HELPERS
-        threshold = -0.15#max(EOD)/2
+        threshold = max(EOD)/2
         f_estimate = calculate_frequency(EOD[::20], sampling_frequency/20, method='spectral')
         frequencies = calculate_frequency(EOD, sampling_frequency, estimated_frequency=f_estimate[0],
                                           crossing_threshold=threshold, method='median', ascending=True)
