2f67b0a62d3497518bf9d34760d1973e0b57ed3d
ElectricFishGirl <berradamia@gmail.com>
2019-09-23 12:26:24 -0400

    Normalizing the fft

--- Raw_data_graphs.py
+++ Raw_data_graphs.py
@@ -20,7 +20,7 @@ for fish in (fish_names):
     shift = 0
    # listy = [0,11,13,14,15]
    # listy = [3,17,18,1,2]
-    for file in mat_files[3:7]:
+    for file in mat_files[6:10]:
         data = helpers.load_mat(file)
         data = data - np.mean(data)
         file_name = helpers.path_to_name(file)
@@ -33,14 +33,14 @@ for fish in (fish_names):
         # xf = np.linspace(0.0, 1.0 / (2.0 * 1 / sampling_frequency), int(len(x) / 2))
         # power = 2.0/len(xf) * np.abs(yf[0:int(len(xf))])
         [xf, power] = helpers.compute_fft(data, 50, helpers.MAT_FREQUENCY)
-        plt.plot(xf, shift+power/10)
+        plt.plot(xf, shift+power/np.max(power))
         shift = shift + 1
         plt.grid()
         plt.ylabel('Amplitude')
         plt.xlabel('Frequency ')
         plt.xlim(0,3000)
         plt.title('FFT ' + file_name )
-    helpers.save_figure(join(helpers.SAVE_PATH, fish), 'FFT 2', fish, file_name)
+    helpers.save_figure(join(helpers.SAVE_PATH, fish), 'FFT 0', fish, file_name)
     plt.close()
 
 
