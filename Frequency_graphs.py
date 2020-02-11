e9948ae94d7f500bd63d6a045c017febdac07020
ElectricFishGirl <berradamia@gmail.com>
2020-02-11 14:51:09 -0500

    improved graphs

--- Frequency_graphs.py
+++ Frequency_graphs.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from os.path import join
 
 fish_names = helpers.get_all_fish(helpers.SAVE_PATH)
-fish = fish_names[3]
+fish = fish_names[9]
 
 CVs = []
 average_frequencies = []
@@ -29,7 +29,7 @@ for numb in file_number:
     data = data[np.isfinite(data)]
     EOD = data - np.mean(data)
     file_name = helpers.path_to_name(mat_files[numb])
-    [frequency, cv, threshold] = helpers.load_npy(npy_files[index])
+    [frequency, cv, threshold] = helpers.load_npy(npy_files[numb])
 
     time_array = helpers.create_time(len(raw_data)-1, style='MAT')
     cycle_time = np.cumsum(1. / frequency)
@@ -67,7 +67,7 @@ for numb in file_number:
 
     ax1 = fig.add_subplot(221)
     ax1.hist((1./frequency)*1e3, bins = 20)
-    ax1.set_title('Histogram of period [ms]')
+    ax1.set_title('Histogram of period [ms]' + ' CV = ' + str(cv))
 
     ax2 = fig.add_subplot(222)
     m13 = frequency
@@ -78,25 +78,16 @@ for numb in file_number:
     ax2.axis('equal')
 
     ax3 = fig.add_subplot(224)
-    ax3.plot(xf,  power/np.max(power))
-    ax3.set_title('FFT' + ' Fundamental at ' + str(fft_main) + 'HZ')
-    ax3.set_ylabel('Amplitude')
-    ax3.set_xlabel('Frequency ')
-    ax3.set_xlim(0, 3000)
+    period = (1. / frequency) * 1e3
+    ax3.acorr(period - period.mean(), usevlines=True, maxlags=10, normed=True, lw=2)
+    ax3.grid(True)
+    ax3.axhline(0, color='black', lw=2)
+    ax3.set_title('Auto-Correlation')
 
     ax4 = fig.add_subplot(223)
     ax4.plot(cycle_time, (1./frequency)*1e3, '.')
-    ax4.set_title('Period with time [ms]' + ' CV = ' + str(cv))
-
+    ax4.set_xlabel('Time [s]')
+    ax4.set_ylabel('Period [ms]')
     helpers.save_figure(join(helpers.SAVE_PATH, fish), 'Analysis V1.1 for ', fish, file_name, 'Analysis_graphs')
     plt.close()
-
-
-
-    # ax3 = fig.add_subplot(223)
-    # ax3.acorr(frequency, usevlines=True, maxlags=50, normed=True, lw=2)
-    # ax3.grid(True)
-    # ax3.axhline(0, color='black', lw=2)
-    # ax3.set_title('Correlation')
-
 
--- Presentation_graphs.py
+++ Presentation_graphs.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from os.path import join
 
-fish = 'Chidi'
+fish = 'Moush'
 
 CVs = []
 average_frequencies = []
@@ -21,20 +21,32 @@ mat_files = helpers.get_mat_files(fish, helpers.RECORDING_PATH16)
 [lowrez_index, highrez_index] = helpers.sort_files(mat_files)
 file_number = highrez_index
 
-numb = 12
+numb = 7
 index = file_number.index(numb)
 raw_data = helpers.load_mat(mat_files[numb])
 data = np.array(raw_data)
 data = data[np.isfinite(data)]
 EOD = data - np.mean(data)
 file_name = helpers.path_to_name(mat_files[numb])
-[frequency, cv, threshold] = helpers.load_npy(npy_files[index])
+[frequency, cv, threshold] = helpers.load_npy(npy_files[numb])
 period = 1./frequency
 time_array = helpers.create_time(len(raw_data)-1, style='MAT')
 cycle_time = np.cumsum(1. / frequency)
 
-[xf, power] = helpers.load_npy(fft_files[index])
-fft_main = 2*power.argmax()
+
+fig = plt.figure('Drugs' + file_name)
+ax1 = fig.add_subplot(222)
+ax1.hist((1./frequency)*1e3, bins = 20)
+ax1.set_title('Histogram of periods ;  ' + ' CV = ' + str(cv))
+ax1.set_xlabel(' period [ms]')
+ax1.set_ylabel('Occurrences ')
+
+ax2 = fig.add_subplot(221)
+period = (1./frequency)*1e3
+ax2.acorr(period-period.mean(), usevlines=True, maxlags=10, normed=True, lw=2)
+ax2.grid(True)
+ax2.axhline(0, color='black', lw=2)
+ax2.set_title('Dose 4 ; Auto-Correlation')
 
 fig = plt.figure('Histogram of ' + file_name)
 plt.hist((1./frequency)*1e3, bins = 20)
@@ -50,19 +62,12 @@ plt.scatter(m13, m12, c=color, marker='.')
 plt.title('Shifted period [ms]')
 plt.axis('equal')
 
-fig3 = plt.figure('FFT' + file_name)
-plt.plot(xf,  power/np.max(power))
-plt.title('FFT' + ' Fundamental at ' + str(fft_main) + 'HZ')
-plt.ylabel('Amplitude')
-plt.xlabel('Frequency ')
-plt.xlim(0, 3000)
 
 fig4 = plt.figure('Period in time' + file_name)
 plt.plot(cycle_time, (1./frequency)*1e3, '.')
 plt.title('Period with time')
 plt.ylabel('Period [ms]')
 plt.xlabel('Time [s] ')
-
 fig5 = plt.figure('Autocorrelation' + file_name)
 period = (1./frequency)*1e3
 plt.acorr(period-period.mean(), usevlines=True, maxlags=10, normed=True, lw=2)
@@ -70,14 +75,4 @@ plt.grid(True)
 plt.axhline(0, color='black', lw=2)
 plt.title('Auto-Correlation')
 
-fig = plt.figure('Drugs' + file_name)
-ax1 = fig.add_subplot(221)
-ax1.hist((1./frequency)*1e3, bins = 20)
-ax1.set_title('Histogram of periods ;  ' + ' CV = ' + str(cv))
-ax1.set_xlabel(' period [ms]')
-ax1.set_ylabel('Occurrences ')
 
-ax2 = fig.add_subplot(222)
-ax2.scatter(m13, m12, c=color, marker='.')
-ax2.set_title('Shifted period [ms]')
-ax2.axis('equal')
